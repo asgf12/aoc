30f6689fa693ab9b0c9e9f78e899ee13cb81309a
asgf12 <142621118+asgf12@users.noreply.github.com>
2025-09-22 23:56:57 +0200

    Created after 1st day of advent of code

---
+++ d1/pt1.ps1
@@ -0,0 +1,36 @@
+#!/usr/bin/env pwsh
+
+# Variables - arrays
+$arr1 = @( )
+$arr2 = @( )
+$first = @( )
+$second = @( )
+# importing data and cleaning into long list (all values in newlines
+$Sample = Get-Content -delimiter ' ' $args
+$data = ($Sample -replace '   ', "`n")
+$datafin = ($data -split "`n")
+
+# loop through data and make arrays (even to first arr)
+for (($i = 0); $i -lt $datafin.Count; $i++){
+    if ($i % 2 -eq 0) {
+# int needed cause of bug with whitespaceses, but it seems to be null divided so 0's out
+        if ([int32]$datafin[$i] -ne 0 ) { $arr1 += [int32]$datafin[$i] }
+    } else {
+        if ([int32]$datafin[$i] -ne 0 ) { $arr2 += [int32]$datafin[$i] }
+} }
+
+# just sorting
+$arr1 | sort-object | foreach {$first += $_}
+$arr2 | sort-object | foreach {$second += $_}
+
+# write-host "A $first`nB $second" # debug
+
+# division and getting absolute value
+for (($j = 0); $j -lt $arr1.Count; $j++){
+    $temp = ($first[$j] - $second[$j])
+        if ($temp -lt 0) {$temp = ( $temp * -1 )}
+    $final += $temp
+}
+
+write-host $final
+# 1223326
