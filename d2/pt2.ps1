f36f41d0ed9e6687c529ee1102002851f687b71c
adi nix 7530 <adrian.lulka@gmail.com>
2025-10-14 21:50:59 +0200

    synchronizing, it works on digits but numbers are not read correctly

--- d2/pt2.ps1
+++ d2/pt2.ps1
@@ -19,12 +19,42 @@ function Ch-init {
 }
 function invalid-mode {
     param ( $par )
-    $Script:TmpArr = new-object -TypeName System.collections.arraylist
-    for (($i = 1); $i -lt ($par.length); $i++) {
-# todo make a loop to populate tmparr, remove does not work
-        # $par.removeat($i) | foreach {$TmpArr.add($_)}
-        Write-host "to check new = $TmpArr"
+    :UWU for (($i = 1); $i -le ($par.length); $i++) {
+        $subvalid = 0
+        $TmpArr = $null
+        $Script:TmpArr = @()
+        $counter = 0
+        for (($j = 0); $j -lt ($par.length); $j++) {
+            if ($counter -ne $i) {
+                $TmpArr+=$par[$j]
+            }
+            $counter++
+        }
 # todo logic to check tmparr, only in func
+# array dont seem to work, echo suggests checking not values, but digits (64>67 checks 6to4)
+        for (($k = 1); $k -le ($TmpArr.length); $k++) {
+            Write-host "to check new = $TmpArr"
+            $invdiff = $TmpArr[($k - 1)] - $TmpArr[$k]
+            if ($incr -eq $true -and $invdiff -in (-1..-3)) {
+                $subvalid++
+                write-host $TmpArr[($k - 1)] - $TmpArr[$k]"$k valid $subvalid"
+                # continue
+                }
+            elseif ($incr -eq $false -and $invdiff -in (1..3)) {
+                $subvalid++
+                write-host $TmpArr[($k - 1)] - $TmpArr[$k]"$k valid $subvalid"
+                # continue
+                }
+            else {
+                write-host "subinvalid"
+                break
+            }
+        if ($subvalid -eq ($TmpArr.length - 1)) {
+            echo safe
+            $Script:final++
+            break UWU
+            }
+        }
     }
 }
 
@@ -32,27 +62,25 @@ foreach ($line in $data) {
     $valid=0
     $arr = new-object -TypeName System.collections.arraylist
     $line.split(" ") | foreach {if ($_ -ne "") {$arr += $_}}
-    invalid-mode $arr
-    exit
-    # $arr | get-member
     write-host "checking $arr"
     Ch-init $arr
         for (($i = 0); $i -lt ($arr.length - 1); $i++) {
         $diff = $arr[$i] - $arr[($i + 1)]
         if ($incr -eq $true -and $diff -in (-1..-3)) {
             $valid++
-            write-host $arr[$i] - $arr[($i + 1)]"$i valid $valid"
+            # write-host $arr[$i] - $arr[($i + 1)]"$i valid $valid"
             # continue
             }
         elseif ($incr -eq $false -and $diff -in (1..3)) {
             $valid++
-            write-host $arr[$i] - $arr[($i + 1)]"$i valid $valid"
+            # write-host $arr[$i] - $arr[($i + 1)]"$i valid $valid"
             # continue
             }
         else {
             Write-host "invalid"
+            invalid-mode $arr
             $incr = $null
-            invalid-mode $arr #  break
+            break
         }
     if ($valid -eq ($arr.length - 1)) {
         echo safe
